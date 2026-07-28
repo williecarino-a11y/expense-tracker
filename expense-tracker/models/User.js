62e1c2f555eaea914d8c67f8bcb8fd76c10395fa
williecarino-a11y <williecarino738@gmail.com>
2026-07-28 19:16:49 +0100

    Update User.js

--- expense-tracker/models/User.js
+++ expense-tracker/models/User.js
@@ -1,28 +1,21 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema(
-  {
+const userSchema = new mongoose.Schema({
     name: {
-      type: String,
-      required: true,
-      trim: true,
+        type: String,
+        required: true,
+        trim: true,
     },
     email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
     },
     password: {
-      type: String,
-      required: true,
-      minlength: 6,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
-
-module.exports = mongoose.model("User", userSchema);
+        type: String,
+        required: true,
+        minlength: 6,
+    }
+}, {
+    timestamps: true
+});
