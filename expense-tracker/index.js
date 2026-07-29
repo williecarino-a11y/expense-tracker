aad0ce9ad5b739be72bf6620bcab77f4129cba2a
Willie <williecarino23@gmail.com>
2026-07-29 09:57:44 +0100

    Fix database connection and configure server

--- expense-tracker/index.js
+++ expense-tracker/index.js
@@ -1,21 +1,23 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routers/authRoutes");
 const expenseRoutes = require("./routers/expenseRoutes");
-const app = express();
-const analyticsRoutes = require("./routers/analyticsRoutes");
 const transactionRoutes = require("./routers/transactionRoutes");
-const subscriptionRoutes = require('./routers/subscriptionRoutes');
+const subscriptionRoutes = require("./routers/subscriptionRoutes");
+const analyticsRoutes = require("./routers/analyticsRoutes");
+
+const app = express();
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-// Replace YOUR_PASSWORD_HERE with your actual password
+// Connect to MongoDB Atlas
 const MONGO_URI = "mongodb+srv://williecarino23_db_user:williecarino070@expense-tracker0.xoeajf4.mongodb.net/?appName=Expense-Tracker0";
 
-// Connect to MongoDB Atlas
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log("Database connected successfully!");
@@ -23,15 +25,16 @@ mongoose.connect(MONGO_URI)
   .catch((err) => {
     console.error("Connection error:", err);
   });
-app.use("/api/analytics", analyticsRoutes);
+
 // Mount Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/subscription", subscriptionRoutes);
+app.use("/api/analytics", analyticsRoutes);
+
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
